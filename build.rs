7f71f29d1054cdb6e2390999ccf55ba7b14516f9
Bradley Hutchinson <bradley.hutchinson@keysight.com>
2022-01-04 11:10:46 -0800

    simplified build.rs

--- build.rs
+++ build.rs
@@ -1,11 +1,7 @@
-#[cfg(target_os = "windows")]
-fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("keysight-logo-gear.ico");
-    res.compile().unwrap();
-}
-
-#[cfg(not(target_os = "windows"))]
-fn main() {
-    // Nothing
+fn main() -> std::io::Result<()> {
+    #[cfg(target_os = "windows")]
+    winres::WindowsResource::new()
+        .set_icon("keysight-logo-gear.ico")
+        .compile()?;
+    Ok(())
 }
