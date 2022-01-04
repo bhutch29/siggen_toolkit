c68f48c93ea835697b04e9f7094bdec335ecee80
Bradley Hutchinson <bradley.hutchinson@keysight.com>
2022-01-04 08:58:00 -0800

    changed import version of winres

--- build.rs
+++ build.rs
@@ -1,7 +1,4 @@
 #[cfg(target_os = "windows")]
-use winres;
-
-#[cfg(target_os = "windows")]
 fn main() {
     let mut res = winres::WindowsResource::new();
     res.set_icon("keysight-logo-gear.ico");
