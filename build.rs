7ef348a368a7d61d927bdfdc48b892006bfc5aad
Bradley Hutchinson <bradley.hutchinson@keysight.com>
2023-09-06 17:37:44 -0700

    removed build.rs

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() -> std::io::Result<()> {
-    // TODO: fails due to missing-file error on Windows
-    // #[cfg(windows)]
-    // winres::WindowsResource::new()
-    //     .set_icon("keysight-logo-gear.ico")
-    //     .compile()?;
-    Ok(())
-}
